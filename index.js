c41302673776bf291664f8703b4eb33973b0d94c
Irfan Arifin <ir.irfan.arifin@gmail.com>
2022-11-27 19:07:04 +0700

    add hook

--- index.js
+++ index.js
@@ -1,50 +1,62 @@
-require('dotenv').config()
 var express = require('express'),
     app = express(),
-    cors = require('cors'),
-    logger = require('morgan'),
     bodyParser = require('body-parser'),
+    axios = require('axios'),
     port = process.env.PORT
 
-app.use(logger('dev'))
+var axios = require('axios')
+var WABA = process.env.URL_WABA
+var KEYWABA = process.env.KEY_WABA
+var NOWA = process.env.NO_WA
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
-var corsOption = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    exposedHeaders: ['x-auth-token'],
-}
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept,  Authorization, x-api-key',
-    )
-    next()
-})
-
-var sampleRoutes = require('./routes/sample.routes')
-var startBot = require('./routes/started.routes')
-var webHook = require('./routes/hook.routes')
-
-app.use('/sample', sampleRoutes)
-app.use('/start', startBot)
-app.use('/webhook', webHook)
-app.use(cors(corsOption))
 
 app.get('/', function (req, res) {
     res.send('OKE')
 })
 
-app.use((req, res) =>
-    res.status(404).json({
-        status: 404,
-        message: 'Request not found',
-    }),
-)
+app.post('/webhook', function (request, response) {
+    let res = JSON.stringify(request.body)
+    let data = JSON.parse(res)
+    axios({
+        method: 'post',
+        url: `${WABA}v1/messages`,
+        headers: {
+            'D360-API-KEY': KEYWABA,
+        },
+        data: {
+            recipient_type: 'individual',
+            to: NOWA,
+            type: 'interactive',
+            interactive: {
+                type: 'button',
+                body: {
+                    text: 'Hallo, Mau ngapain ?',
+                },
+                action: {
+                    buttons: [
+                        {
+                            type: 'reply',
+                            reply: {
+                                id: '1',
+                                title: 'Engga ngapa2in',
+                            },
+                        },
+                        {
+                            type: 'reply',
+                            reply: {
+                                id: '2',
+                                title: 'Belanja dong!',
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+    })
+    response.sendStatus(200)
+})
 
 app.listen(port, function () {
     console.log(`App is listening on port ${port}`)
