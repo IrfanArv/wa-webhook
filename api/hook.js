09ff253f8965ae0d5650b803739f5f312047fd78
Irfan Arifin <ir.irfan.arifin@gmail.com>
2022-11-27 10:11:43 +0700

    test hook-5

--- api/hook.js
+++ api/hook.js
@@ -23,15 +23,22 @@ module.exports = {
                     },
                 },
             })
-            // let data = JSON.parse(res)
-            // let buttonReplay = data.messages[0].interactive.button_reply.id
-            // if ((buttonReplay = 1)) {
-            //     console.log('Beli')
-            // }
-            // if ((buttonReplay = 2)) {
-            //     console.log('Engga Beli')
-            // }
-            res.sendStatus(200)
+                // let data = JSON.parse(res)
+                // let buttonReplay = data.messages[0].interactive.button_reply.id
+                // if ((buttonReplay = 1)) {
+                //     console.log('Beli')
+                // }
+                // if ((buttonReplay = 2)) {
+                //     console.log('Engga Beli')
+                // }
+                .then(() => {
+                    const response = {
+                        status: 200,
+                        success: true,
+                        message: 'Terkirim',
+                    }
+                    return res.status(200).send(response)
+                })
         } catch (err) {
             let response = {
                 status: 400,
